74b1448ca6a5b38390cfa241c8b94751d900b3eb
olgamartinchik <sintcerova@mail.ru>
2022-01-18 21:43:00 +0300

    init: moving backend from rsclon repository to rsclon-backend repository

---
+++ app.ts
@@ -0,0 +1,41 @@
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(require("morgan")("dev"));
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+// app.use(cors());
+
+app.use(express.json({ extended: true }));
+
+//initialize routs
+app.use("/api/auth", require("./routes/auth.routes"));
+
+const PORT = process.env.PORT || config.get("port");
+
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUri"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`App has been started on port ${PORT}...`)
+    );
+  } catch (e) {
+    console.log("Server Error", e.message);
+    process.exit(1);
+  }
+}
+
+start();
---
+++ dist/model/User.js
@@ -0,0 +1,9 @@
+const { Schema, model, Types } = require("mongoose");
+const schema = new Schema({
+    userName: { type: String, required: true, unique: false, trim: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    // id:''
+});
+module.exports = model("User", schema);
+//# sourceMappingURL=User.js.map
