ade93e6bc6c33d3424942d0260875c4b4145310f
olgamartinchik <sintcerova@mail.ru>
2023-01-05 12:52:47 +0300

    Delete dist directory

--- dist/model/UserMenu.js
+++
@@ -1,8 +0,0 @@
-const mongooseUM = require('mongoose');
-const SchemaUM = mongooseUM.Schema;
-const schemaUM = new SchemaUM({
-    _id: String,
-    periodUserMeal: Object,
-});
-module.exports = mongooseUM.model('UserMenu', schemaUM);
-//# sourceMappingURL=UserMenu.js.map
--- dist/model/WorkoutSettings.js
+++
@@ -1,8 +0,0 @@
-const mongooseWS = require('mongoose');
-const SchemaWS = mongooseWS.Schema;
-const schemaWS = new SchemaWS({
-    _id: String,
-    program: [[Object]],
-});
-module.exports = mongooseWS.model('WorkoutSettings', schemaWS);
-//# sourceMappingURL=WorkoutSettings.js.map
